699852b4df41440339b9469e7652055dc6154c3f
CH Lim <chl@chl.com>
2020-05-07 22:21:20 +0800

    Version 0.2
    Displays a list of tables found in the database.
    - includes the name of the fields.

--- browse-db.py
+++ browse-db.py
@@ -1,10 +1,10 @@
 import sqlalchemy
 import sys
 
-VERSION = 0.1
+VERSION = 0.2
 
 def welcome_msg():
-    print(f'Welcome to db-browser version {VERSION}\n\n')
+    print(f'\nWelcome to db-browser version {VERSION}\n\n')
 
 def input_db_path():
     # retrive db path provided as a argument
@@ -30,12 +30,31 @@ def run_queries(engine):
     
     print('end')
 
+def show_table(table):
+    print(table.name)
+    for column in table.c:
+        print(f'\t{column.name}')
+
+    # print blankline
+    print() 
+
+def show_schema(engine):
+    metadata = sqlalchemy.MetaData()
+    metadata.reflect(bind=engine)
+
+    print('## TABLES\n')
+
+    for table in metadata.tables.values():
+        show_table(table)
+
+
 def main():
     welcome_msg()
 
     db_path = input_db_path()
     engine = sqlalchemy.create_engine(db_path)
 
+    show_schema(engine)
     run_queries(engine)
 
 
